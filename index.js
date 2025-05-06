f98a8e211f6bf622e9a28bae39406d8d19c23248
Nicole Åhgren <na223jy@student.lnu.se>
2025-05-06 15:44:58 +0200

    liten ändring

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const app = express()
 const port = 2002
@@ -16,10 +15,10 @@ app.get('/views/:filename', (req, res) => {
 const upload = multer({ dest: 'uploads/' })
 
 app.post("/submit-form", upload.single("cv"), async (req, res) => {
-  const { name, email, message } = req.body;
-  const cvFile = req.file;
+  const { name, email, message } = req.body
+  const cvFile = req.file
 
-  require("dotenv").config();
+  require("dotenv").config()
 
   // Konfigurera e-post
   const transporter = nodemailer.createTransport({
@@ -28,26 +27,23 @@ app.post("/submit-form", upload.single("cv"), async (req, res) => {
           user: process.env.EMAIL,
           pass: process.env.PASSWORD
       }
-  });
+  })
   const mailOptions = {
     from: email,
     to: "kontakt@xanthe.se",
     subject: `Jobbansökan från ${name}`,
     text: message,
     attachments: [{ path: cvFile.path }]
-};
+}
 
 try {
-    await transporter.sendMail(mailOptions);
-    res.send("Ansökan skickad!");
+    await transporter.sendMail(mailOptions)
+    res.send("Ansökan skickad!")
 } catch (error) {
-    console.error(error);
-    res.status(500).send("Fel vid e-posthantering.");
+    console.error(error)
+    res.status(500).send("Fel vid e-posthantering.")
 }
-});
-
-
-
+})
 
 app.listen(port, () => {
     console.log(`Servern körs på http://localhost:${port}`)
