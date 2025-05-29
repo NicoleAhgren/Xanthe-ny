f89bfbfb146e25ff28f2f2574d71b2577d98b1eb
Nicole Åhgren <na223jy@student.lnu.se>
2025-05-29 19:07:12 +0200

    små ändringar

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const app = express();
-const port = 2002;
-const path = require("path");
-const multer = require("multer");
-const nodemailer = require("nodemailer");
+const express = require("express")
+const app = express()
+const port = 2002
+const path = require("path")
+const multer = require("multer")
+const nodemailer = require("nodemailer")
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/views/:filename", (req, res) => {
-  res.sendFile(path.join(__dirname, "src/views", req.params.filename));
-});
+  res.sendFile(path.join(__dirname, "src/views", req.params.filename))
+})
 
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: "uploads/" })
 
 app.post("/submit-form", upload.single("cv"), async (req, res) => {
-  const { name, email, message } = req.body;
-  const cvFile = req.file;
+  const { name, email, message } = req.body
+  const cvFile = req.file
 
-  require("dotenv").config();
+  require("dotenv").config()
 
   // Konfigurera e-post
   const transporter = nodemailer.createTransport({
@@ -29,7 +29,7 @@ app.post("/submit-form", upload.single("cv"), async (req, res) => {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
     },
-  });
+  })
   const mailOptions = {
     from: process.env.EMAIL,
     replyTo: email,
@@ -37,17 +37,17 @@ app.post("/submit-form", upload.single("cv"), async (req, res) => {
     subject: `Jobbansökan från ${name}`,
     text: message,
     attachments: [{ path: cvFile.path }],
-  };
+  }
 
   try {
-    await transporter.sendMail(mailOptions);
-    res.send("Ansökan skickad!");
+    await transporter.sendMail(mailOptions)
+    res.send("Ansökan skickad!")
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Fel vid e-posthantering.");
+    console.error(error)
+    res.status(500).send("Fel vid e-posthantering.")
   }
-});
+})
 
 app.listen(port, () => {
-  console.log(`Servern körs på http://localhost:${port}`);
-});
+  console.log(`Servern körs på http://localhost:${port}`)
+})
